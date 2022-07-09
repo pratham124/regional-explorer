4b2e5b851a844d6d959e7f5787ba0eb3584fd50d
Pratham Sitoula <psitoula2003@gmail.com>
2022-07-09 16:54:17 -0600

    CSS complete

--- dist/index.62406edb.js
+++ dist/index.62406edb.js
@@ -1,4 +1,4 @@
-const recipeContainer = document.querySelector(".recipe");
+const countryContainer = document.querySelector(".country");
 const timeout = function(s) {
     return new Promise(function(_, reject) {
         setTimeout(function() {
